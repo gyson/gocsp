f9d4491db481cf381196722c642460014fa60926
gyson <eilian.yunsong@gmail.com>
2014-03-05 11:00:56 -0500

    add browser support

--- examples/try.js
+++ examples/try.js
@@ -1,16 +1,12 @@
 
 var go = require("gocsp");
 
-var ch = new go.Channel();
-try {
-	go.safe(function* () {
-		yield ch;
-	});
-
-} catch (e) {
-
-	console.log("catch: ", e)
-}
 
 
-ch.throw("kkkkkk");
+go(function* () {
+	console.log(yield go.require("hello"));
+
+	console.log(yield go.load("hello"));
+})
+
+go.define("hello", " return 'dddd' ");
---
+++ index.js
@@ -0,0 +1,8 @@
+
+require("./lib/go.js");
+require("./lib/util.js");
+require("./lib/module.js");
+require("./lib/web.js");
+
+// go is global variable now.
+module.exports = go;
