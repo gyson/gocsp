136e018e8c14d435987ea822d85fbe14c072095d
gyson <eilian.yunsong@gmail.com>
2014-10-09 02:18:01 +0800

    move http to gocsp-http

--- test/http-listen.js
+++
@@ -1,9 +0,0 @@
-
-var go = require('../lib/index')
-var http = require('../lib/http')
-
-new go.Channel(http.listen(8080))
-    .each(function (conn) {
-        console.log('new connection')
-    })
-    .done()
--- test/http-request.js
+++
@@ -1,9 +0,0 @@
-
-var go = require('../lib/index')
-var http = require('../lib/http')
-
-var req = new go.Channel(http.request('http://localhost:8080'))
-
-req.close()
-
-req.each(console.log).done()
