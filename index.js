eaade2f39d78c26572531898bb786bd8faae61a0
gyson <eilian.yunsong@gmail.com>
2014-10-22 18:12:01 +0800

    clean up, remove parts into their own repos

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 
-'use strict';
+'use strict'
 
-module.exports = require('./lib/index');
+exports.co = require('gocsp-co')
+exports.select = require('gocsp-select')
+exports.Channel = require('gocsp-channel')
+
+var thunk = exports.thunk = require('gocsp-thunk')
+
+exports.take = function (chan) {
+    return thunk(function (cb) {
+        chan.take(function (res) {
+            cb(null, res)
+        })
+    })
+}
+
+exports.put = function (chan, value) {
+    return thunk(function (cb) {
+        chan.put(value, function (res) {
+            cb(null, res)
+        })
+    })
+}
--- test/coroutine.js
+++
@@ -1,4 +0,0 @@
-
-var go = require('gocsp')
-var util = require('util')
-var assert = require('assert')
